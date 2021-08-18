1be1ffe33dcf7a53af8d044bffa53e3da0abcf26
devsacti <devsacti@gmail.com>
2021-08-18 21:49:22 +0900

    warmup

--- PSrecords_python/PS-WarmUp/step2/bfs-dfs/수도 배관.py
+++ PSrecords_python/PS-WarmUp/step2/bfs-dfs/수도 배관.py
@@ -5,7 +5,11 @@
 # 마지막 지점의 번호는 항상 1이다. 즉, 루트노드의 번호는 1이다
 # 이런 조건이 없었다면, reversed_tree를 dfs로 해서 부모노드가 없을때까지 탐색
 
-# 한번의 순회를 통해 inner와 leaf를 도출하고, 첫방문노드에 1을 기준으로 부모와 자식 노드들의 할당비를 
+# 두 지점이 서로 연결되어있다는 의미 >> 부모자식이랑 별개의 무방향 간선 정보로서 층위와 무관하게 줘서 별도 처리 필요
+## 우선 인풋값 받을 당시엔 무엇이 부모인지 알수없어, 양방향으로 연결하되,
+## 1번이 루트라는 조건에 따라 1번부터 살펴보고, 부모로의 방향은 끊으면 된다.
+
+# 한번의 순회를 통해 inner와 leaf를 도출하고, 첫방문노드에 1을 기준으로 부모와 자식 노드들의 할당비를
 # 구할 수 없을까 고민했지만, 유리수의 문제가 너무 컸다. 즉, leaf에 할당비를 1로 두고 해야 오차가 없다.
 
 ## utils-integ algo
@@ -24,78 +28,96 @@
 from collections import defaultdict
 from collections import deque
 
-# visited 대신 중복방문 허용 dfs
-def dfs(tree,v,weights):
-  
-  if(weights[v]==0):
-    weights[v]=1
-  else:
-    weights[v]+=1
-  
 
-  for adj in tree[v]:
-    dfs(tree,adj,weights)
-  
+# dfs for finding leaf of tree
+def is_leaf(tree, v, visited, leafs, weights):
+    if (len(tree[v]) == 0):
+        leafs.append(v)
+        weights[v] = 1
 
-def bfs(tree,s,visited,inners,leafs):
-  visited[s]=1
-  q=deque()
-  q.append(s)
-  
-  while q:
-    now=q.popleft()
-    
-    # root is given as 1
-    # inner vs leaf
-    if(now != 1):
-      if(len(tree[now])==0):
-        leafs.append(now)
-      else:
-        inners.append(now)
-        
-    for adj in tree[now]:
-      if(visited[adj]==0):
-        visited[adj]=1
-        q.append(adj)
-        
-  
-  return inners,leafs
+    for adj in tree[v]:
+        if (visited[adj] == 0):
+            is_leaf(tree, adj, visited, leafs, weights)
 
-if __name__=="__main__":
-  n,b=map(int,input().split())
-  
-  tree=defaultdict(list)
-  reversed_tree=defaultdict(list)
-  
-  for _ in range(n-1):
-    parent, child = map(int,input().split())
-    
-    tree[parent].append(child)
-    
-    reversed_tree[child].append(parent)
-  print(tree)
-  print(reversed_tree)
-  
-  visited=['*']+[0 for _ in range(n)]
-  inner_nodes=[]
-  leaf_nodes=[]
-  
-  inner_nodes,leaf_nodes=bfs(tree,1,visited,inner_nodes,leaf_nodes)
-  
-  print(inner_nodes)
-  print(leaf_nodes)
-  
-  weights=[0]+[0 for _ in range(n)]
-  
-  # 도출된 leaf들마다 dfs
-  for leaf in leaf_nodes:
-    dfs(reversed_tree,leaf,weights)
-    
-  # print(weights)
-  
-  div=sum(weights)
-  
-  # if(b%div==0):
-  #   print(b//div)
-  # else:
-  #   print(-1)
+
+# directed edge, so visited don't needed
+def dfs(reverse, v, weights):
+    for adj in reverse[v]:
+        weights[adj] += 1
+        dfs(reverse, adj, weights)
+
+
+# bfs for making tree from graph
+def make_tree(graph, s, visited, tree, reversed_tree):
+    visited[s] = 1
+    q = deque()
+    q.append(s)
+
+    while q:
+        now = q.popleft()
+
+        # 양방향 그래프라 여기는 안됨
+        # tree[now].extend(graph[now])
+
+        for adj in graph[now]:
+            if (visited[adj] == 0):
+                # 현재 기준 방문안한 애들이 자식임
+                tree[now].append(adj)
+                visited[adj] = 1
+                q.append(adj)
+            else:
+                reversed_tree[now].append(adj)
+
+    return tree, reversed_tree
+
+
+if __name__ == "__main__":
+    n, b = map(int, input().split())
+
+    graph = defaultdict(list)
+
+    # 간선 정보다 층위와 별개라 v1,v2로 정정
+    for _ in range(n - 1):
+        v1, v2 = map(int, input().split())
+        # 무방향 그래프로 우선 선언
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+    keys = sorted(list(graph))
+
+    # for k in keys:
+    #   print(k,':',graph[k])
+
+    tree = defaultdict(list)
+    reversed_tree = defaultdict(list)
+
+    visited = ['*'] + [0 for _ in range(n)]
+
+    tree, reversed_tree = make_tree(graph, 1, visited, tree, reversed_tree)
+
+    # print(tree)
+    # print(reversed_tree)
+
+    # tree와 reversed_tree의 관계를 이용해서 아래를 찾을 수도 있는데 O(n^2)라 지양
+    leaf_nodes = []
+    visited = ['*'] + [0 for _ in range(n)]
+    weights = [0] + [0 for _ in range(n)]
+
+    is_leaf(tree, 1, visited, leaf_nodes, weights)
+
+    # print(leaf_nodes)
+    # print(weights)
+
+    # 도출된 leaf들마다 dfs
+    for leaf in leaf_nodes:
+        dfs(reversed_tree, leaf, weights)
+
+    # print(weights)
+
+    div = sum(weights)
+    # print(div)
+
+    if (b % div == 0):
+        print(b // div)
+    else:
+        print(-1)
