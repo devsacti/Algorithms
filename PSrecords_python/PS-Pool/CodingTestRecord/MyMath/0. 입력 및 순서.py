409256a4e63e4f06fa243c7214fe3afbf598da69
trailblazer wannabe <88543657+devsacti@users.noreply.github.com>
2021-08-21 22:58:36 +0900

    Delete PSrecords_python/PS-Pool/CodingTestRecord directory

--- PSrecords_python/PS-Pool/CodingTestRecord/MyMath/0. 입력 및 순서.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-
-seq=[]
-
-for i in range(n):
-    seq.append(input())
-
-print('--')
-print(' '.join(seq))
--- PSrecords_python/PS-Pool/CodingTestRecord/MyMath/DPplus/동전줍기.py
+++
@@ -1,6 +0,0 @@
-
-row = list(map(int,input().split()))
-
-row.sort()
-
-print(row[-1]-row[0])
