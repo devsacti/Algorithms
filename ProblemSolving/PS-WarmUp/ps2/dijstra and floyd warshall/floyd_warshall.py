c863de5a6f554bca26ee0bbaf4f2ef8605a4c949
devsacti <devsacti@gmail.com>
2022-01-13 23:48:32 +0900

    update ps

--- ProblemSolving/PS-WarmUp/ps2/dijstra and floyd warshall/floyd_warshall.py
+++ ProblemSolving/PS-WarmUp/ps2/dijstra and floyd warshall/floyd_warshall.py
@@ -23,6 +23,9 @@ ps1.2. drawing pattern, exceptions
 그리고 이렇게 삼각형의 사이클을 만들다 보면 결국 모든 다각형은 3각형으로 분할가능하므로,
 연결된 노드들 간에는 최단거리를 도출가능해지지 않나라고 이해해봤다.
 
+하지만 역시나 스스로에게 충분히 와닿지 않는다. 
+그러나 구현이 이제는 더 중요하므로, 차차 스스로의 의문과 관련된 예시를 만들어 해결해보자
+그리고 구현하면서 다시 보면 또 되기도 하고
 
 ps2. applying computer algorithms to comprehension
 ps2.1. utilizing and modularizing
@@ -30,56 +33,40 @@ ps2.2. integration
 
 ps3.Impl
 '''
-from collections import defaultdict
-from heapq import *
+def floyd_warshall():
+      global n,shortests
 
-def floyd_warshall(graph, s, shortests):
-  shortests[s]=0
-  pq=[]
-  heappush(pq,(0,s))
-  
-  while pq:
-      # 이전 탐색을 통해 도출한 현재 지점까지의 최단거리 후보와 현재 노드
-      cand_shortest,now=heappop(pq)
+      for k in range(n):
+            for s in range(n):
+                  for e in range(n):
+                        shortests[s][e]=min(shortests[s][e], shortests[s][k]+shortests[k][e])
       
-      # 문제는 최초의 노드_v0_에서 인접노드들_가령, v1,v2_로 접근할때, 인접노드들 간에 연결 되어있을 수 있다는 것이다.
-      # 이 때 최초 노드에서 업데이트한 최단거리_v0 에서 v1_가 v2에서 v1으로 접근할때 최단거리보다 작다면, 최단거리로서 가치가 없고
-      # 이를 제대로 예외처리하지 않는다면, 추후 최단거리 산출에서 오차가 생기므로 이렇게 스킵한다.
-      if(cand_shortest>shortests[now]):continue
-    
-      for adj in graph[now]:
-        interval, next = adj
-        # 지금까지 온 거리 + 다음 인접 노드까지의 거리로 최소 후보값 정의
-        cand_shortest=shortests[now]+interval
-        
-        if(cand_shortest<shortests[next]):
-            # if문 안에 들어온 순간 cand_shortest shortest
-            shortests[next]=cand_shortest
-            heappush(pq,(cand_shortest,next))
-
+      # print()
+      # print(*shortests,sep='\n')
 
 if __name__=="__main__":
-  n,m=map(int,input().split())
-  
-  inf=int(1e10)  
-  
-  graph=defaultdict(list)
-  
-  for _ in range(m):
-    v1, v2 = map(int, input().split())
-    
-    # (interval between start and destination, dest vertex)
-    graph[v1].append((1,v2))
-    graph[v2].append((1,v1))
-  
-  s,e =map(int,input().split())
-  
-  # for k in sorted(list(graph)): 
-  #   print(k,graph[k])
-    
-  shortests=[inf for _ in range(n)]
-  
-  dijstra(graph,s,shortests)
-  
-  print(shortests[e])
-  
+      n,m,k=map(int,input().split())
+
+      inf=int(1e9)
+      # for floyd-warshall
+      global shortests
+      shortests=[[inf]*n for _ in range(n)]
+
+      for _ in range(m):
+        v1, v2, cost = map(int, input().split())
+
+        # 2d shortests
+        shortests[v1-1][v1-1]=0
+        shortests[v1-1][v2-1]=cost
+        shortests[v2-1][v2-1]=0
+      
+      # print(*shortests, sep='\n') 
+
+      floyd_warshall()
+
+      total=0
+
+      for s in range(n):
+        total+=(shortests[s][k-1]+shortests[k-1][s])
+      
+      print(total)
