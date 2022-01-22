8bf1fb67c6b6033d74a15aee1a18e2c8d58a2a28
devsacti <devsacti@gmail.com>
2022-01-22 22:53:08 +0900

    to be refactoring Recursive factorial

--- Algorithms/java/chapter05_DFSBFS/Recursive/Main.java
+++ Algorithms/java/chapter05_DFSBFS/Recursive/Main.java
@@ -1,5 +1,9 @@
 import java.util.*;
+/*
 
+팩토리얼과 재귀구조, 그리고 백트래킹
+
+*/
 public class Main {
 
     // 반복적으로 구현한 n!
@@ -20,10 +24,18 @@ public class Main {
         return n * factorialRecursive(n - 1);
     }
 
+    // Recursive and Backtracking
+    // 당연히 비효율적이지만 Recursive 구조에서 응용가능한 부분을 체크한다.
+
+    public static int factorialRecursiveAndBacktracking(int i){
+        return 0;
+    }
+
     public static void main(String[] args) {
         // 각각의 방식으로 구현한 n! 출력(n = 5)
         System.out.println("반복적으로 구현:" + factorialIterative(5));
         System.out.println("재귀적으로 구현:" + factorialRecursive(5));
+        System.out.println("재귀+백트래킹으로 구현:" + factorialRecursive(5));
     }
 
 }
--- Algorithms/java/test/Main.java
+++ Algorithms/java/test/Main.java
@@ -9,25 +9,20 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
-        // 2차원 '배열(array)' 방식 입력
-        int cnt_row=3;
-        int cnt_column=4;
-        int[][] matrix = new int[cnt_row][cnt_column];
-        sc.close();
-        for (int r=0;r<cnt_row;r++){
-            for (int c=0;c<cnt_column;c++){
-                matrix[r][c]=sc.nextInt();
-            }
+
+        int i=sc.nextInt();
+
+        int constant = i;
+        // constant = i + j 
+        // => constant - j = i
+        int j = 0; //차감하는 수
+
+        for(int cnt_case=0;cnt_case<constant;cnt_case++){
+            int current=constant-j;
+            System.out.println(current);
+            // 차감하는 변수 j
+            j++;
         }
 
-        // 2차원 '배열(array)' 방식 출력
-        System.out.println();
-        for (int r=0;r<cnt_row;r++){
-            for (int c=0;c<cnt_column;c++){
-                System.out.printf("%d ",matrix[r][c]);
-            }
-            System.out.println();
-        }
     }
 }
