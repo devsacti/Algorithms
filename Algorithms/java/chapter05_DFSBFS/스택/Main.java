5187a8de1b0b0f01dabaf65b6e53e589bb332659
devsacti <devsacti@gmail.com>
2022-01-23 23:06:18 +0900

    update java stack,queue

--- Algorithms/java/chapter05_DFSBFS/스택/Main.java
+++ Algorithms/java/chapter05_DFSBFS/스택/Main.java
@@ -1,10 +1,36 @@
 import java.util.*;
 
+/*
+<python>
+(no import)
+stack=[]
+append, pop
+<java>
+import java.util.*;
+Stack<Integer> s = new Stack<>();
+push, pop
+
+*/
+
 public class Main {
 
     public static void main(String[] args) {
+        // AS-IS, python
+        // stack=[]
+        // TO-BE
         Stack<Integer> s = new Stack<>();
 
+        // AS-IS, python
+        // stack.append(5)
+        // stack.append(2)
+        // stack.append(3)
+        // stack.append(7)
+        // stack.pop()
+        // stack.append(1)
+        // stack.append(4)
+        // stack.pop()
+
+        // TO-BE
         // 삽입(5) - 삽입(2) - 삽입(3) - 삽입(7) - 삭제() - 삽입(1) - 삽입(4) - 삭제()
         s.push(5);
         s.push(2);
--- Algorithms/java/chapter05_DFSBFS/큐/Main.java
+++ Algorithms/java/chapter05_DFSBFS/큐/Main.java
@@ -1,5 +1,18 @@
 import java.util.*;
+/*
 
+<python>
+from collections import deque
+q=deque()
+q.append, q.popleft
+
+<java>
+import java.util.*;
+Queue<Integer> q = new LinkedList<>();
+q.offer,q.poll
+
+왜 poll일까? 왠지 투표함을 까보는 느낌이 아닐까함. 그래서 맨 위 표를 개표하는 느낌
+*/
 public class Main {
 
     public static void main(String[] args) {
