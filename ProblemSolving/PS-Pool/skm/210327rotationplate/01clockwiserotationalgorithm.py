63561f359c0116a44e60a5839d62eae7f7fdb195
devsacti <devsacti@gmail.com>
2022-01-31 21:37:03 +0900

    separate algorithms and ps

--- ProblemSolving/PS-Pool/skm/210327rotationplate/01clockwiserotationalgorithm.py
+++
@@ -1,8 +0,0 @@
-if __name__=="__main__":
-    n=int(input())
-    seqs=list(map(int,input().split()))
-    idx_s, interval=map(int,input().split())
-
-    # print(n, seqs,idx_s,interval)
-
-    print( ( seqs[(idx_s+(interval))%n] ))
