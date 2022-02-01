dcedb6296b87f6e45c556fe418351ad30ef07f8c
devsacti <devsacti@gmail.com>
2022-02-01 23:58:38 +0900

    update java algorithms

--- java/chapter06_Sorting/6장 정렬/위에서 아래로/Main.java
+++ java/chapter06_Sorting/6장 정렬/위에서 아래로/Main.java
@@ -1,5 +1,17 @@
 import java.util.*;
 
+/*
+example InOut
+
+3
+15
+27
+12
+--
+27 15 12
+
+*/
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,6 +27,9 @@ public class Main {
         }
 
         // 기본 정렬 라이브러리를 이용하여 내림차순 정렬 수행
+        // 파이썬은 리스트 객체 자체에 정렬 기능이 탑재되어있다고 볼 수 있다.
+        // arr.sort(reverse=True)
+        // 하지만 java는 외부의 도움을 받아야하기에 어딘가로 던져져야 한다고 이해할 수 있다.
         Arrays.sort(arr, Collections.reverseOrder());
 
         for(int i = 0; i < n; i++) {
